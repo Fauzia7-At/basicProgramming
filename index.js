fa4db78d36ef3377c3881037416cba37abcabe63
Fauzia7-At <h.ftabassum7@gmail.com>
2024-12-31 13:19:21 +0530

    basic

---
+++ index.js
@@ -0,0 +1,13 @@
+function Primenum(num){
+    if(num<=1){
+       return false; 
+    }
+    for(let i =2;i<= Math.sqrt(num);i++){
+        if(num%1==0){
+            return false;
+        // }found dividor not a prime
+    }
+ return true; //no divisor found return prime
+    }
+   
+console.log(8);
